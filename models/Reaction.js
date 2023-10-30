16fd78adccbca60961adc740c82c8412c680fd80
dmerk2 <dan.merkin@gmail.com>
2023-10-30 18:25:06 -0400

    Creating models

---
+++ models/Reaction.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const reactionSchema = new mongoose.Schema(
+  {
+    reactionId: {
+      type: mongoose.Schema.Types.ObjectId,
+      default: () => new mongoose.Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+reactionSchema.path("createdAt").get(function (value) {
+  return new Date(value).toISOString();
+});
+
+module.exports = reactionSchema;
---
+++ models/User.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+    },
+    thoughts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Define the virtual field
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+module.exports = User;
